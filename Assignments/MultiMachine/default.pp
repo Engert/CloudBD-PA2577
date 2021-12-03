a20f458f7dde435d0a09725e5f1aa7f21daeab60
Engert <38441414+Engert@users.noreply.github.com>
2021-12-03 12:02:33 +0100

    Add files via upload

---
+++ Assignments/MultiMachine/default.pp
@@ -0,0 +1,58 @@
+#exec { "apt-update":
+#  command => "/usr/bin/apt-get update"
+#}
+
+
+#$packages = ["curl", "nodejs"]
+#package {
+#  $packages: ensure => installed,
+#  require => Exec["apt-update"],
+#  require => require => Exec["Add nodesource sources"],
+#}
+
+
+#appserver has node.js installed
+#dbserver has mysql installed and running
+#web has nginx installed and running
+#tst0 -- tst2 have simply run apt-get update and nothing more.
+
+node /^tst\d+$/ {
+  exec { "apt-update":
+    command => "/usr/bin/apt-get update" }
+}
+
+node 'appserver' {
+  exec { "Add nodesource sources":
+    command => 'curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -',
+    creates => '/etc/apt/sources.list.d/nodesource.list',
+    path    => ['/usr/bin', '/bin', '/usr/sbin']; }
+  include nodejs
+}
+
+node 'dbserver' {
+  include mysql
+}
+
+node 'web' {
+  include nginx
+}
+
+node default {
+  notify { 'this node did not match any of the listed definitions': }
+}
+
+#package { 'curl':
+#  ensure => installed,
+#  require => Exec["apt-update"],
+#}
+
+#exec { "Add nodesource sources":
+#  command => 'curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -',
+#  creates => '/etc/apt/sources.list.d/nodesource.list',
+#  path    => ['/usr/bin', '/bin', '/usr/sbin'];
+#}
+
+#package { 'nodejs':
+#  ensure => installed,
+#  require => Exec["Add nodesource sources"];
+#}
